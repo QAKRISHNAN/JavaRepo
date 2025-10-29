c5ef0fc12ff62c80b876e6e7bc70907a378f8f73
QAKRISHNAN <tronixtechkrishna@gmail.com>
2025-10-29 12:29:18 +0530

    Initial commit

---
+++ Addition2.java
@@ -0,0 +1,14 @@
+class Addition2Num
+{
+public static void main(String args[])
+{
+int a=5;
+int b=5
+int c;
+c=a+b;
+System.out.print("Addition of given Two Numbers:");
+System.out.println(c);
+}
+}  
+
+
